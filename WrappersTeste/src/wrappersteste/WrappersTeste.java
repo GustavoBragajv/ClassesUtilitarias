3b80671d16ded852dd0d80f8f2d319a6fec98075
higor <higor@ubuntu-linux>
2024-03-24 23:52:09 -0300

    Estudando conceitos sobre Classes Utilitarias em java (Wrappers)

---
+++ WrappersTeste/src/wrappersteste/WrappersTeste.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package wrappersteste;
+
+/**
+ *
+ * @author higor
+ */
+public class WrappersTeste {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Tipos Primitivos");
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10L;
+        float floatP = 10f;
+        double doubleP = 10D;
+        char charP = 'h';
+        boolean booleanP = true;
+        
+        System.out.println("Classes Wrappers");
+        Byte byteW = 1;
+        Short shortW = 1;
+        Integer intW = 1;
+        Long longW = 10L;
+        Float floatW = 10f;
+        Double doubleW = 10D;
+        Character charW = 'h';
+        Boolean booleanW = true;
+    }
+    
+}
