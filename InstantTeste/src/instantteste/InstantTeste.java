4a2f0724db418303b9c914d27f8e2e708dd99fab
higor <higor@localhost>
2024-03-27 13:33:09 +0100

    Estudando conceitos sobre a Classe Instant que também pode ser usada para trabalhar com datas e horas.

---
+++ InstantTeste/src/instantteste/InstantTeste.java
@@ -0,0 +1,14 @@
+package instantteste;
+
+import java.time.Instant;
+
+
+public class InstantTeste {
+
+    public static void main(String[] args) {
+        // Trabalha com nano segundos.
+        Instant agora = Instant.now();
+        System.out.println(agora);
+    }
+
+}
