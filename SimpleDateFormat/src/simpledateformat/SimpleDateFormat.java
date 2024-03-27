367c87eee0da73b4f2b4dc4f07a80814a25f6177
higor <higor@localhost>
2024-03-27 12:27:06 +0100

    Atualizando os estudos sobre SimpleDateFormat

--- SimpleDateFormat/src/simpledateformat/SimpleDateFormat.java
+++ SimpleDateFormat/src/simpledateformat/SimpleDateFormat.java
@@ -1,5 +1,6 @@
 package simpledateformat;
 
+import java.text.ParseException;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 
@@ -10,6 +11,11 @@ public class SimpleDateFormat {
         String pattern = "'Hamburgo' dd 'de' MMMM 'de' YYYY";
         SimpleDateFormat sdf = new SimpleDateFormat(pattern);
         System.out.println(sdf.format(new Date()));
+        try{
+            System.out.println(sdf.parse("Hamburgo 27 de Marsch de 2024"));
+        } catch(ParseException e) {
+            e.printStackTrace();
+        }
     }
 
 }
