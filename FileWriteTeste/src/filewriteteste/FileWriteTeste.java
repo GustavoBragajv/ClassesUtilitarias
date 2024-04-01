37d47eee39c5925a6b38e08a14272b25697083e4
higor <higor@localhost>
2024-04-01 09:19:53 +0200

    Modificando programa file writer...

--- FileWriteTeste/src/filewriteteste/FileWriteTeste.java
+++ FileWriteTeste/src/filewriteteste/FileWriteTeste.java
@@ -10,11 +10,12 @@ public class FileWriteTeste {
     public static void main(String[] args) {
         File file = new File("file.txt");
         try(FileWriter fw = new FileWriter(file, true)) {
-            fw.write("Eu serei um programador e irei morar na alemanha");
+            fw.write("Eu serei um programador e irei morar na alemanha\n");
             fw.flush();
         } catch (IOException exception) {
             exception.printStackTrace();
         }
+        
     }
 
 }
