aa6070afe7e958600b58b30c9e72c4cb1c7fcff1
higor <higor@localhost>
2024-04-01 08:42:17 +0200

    Estudando conceitos sobre Entrada e saida de dados IO

---
+++ filewirteteste/FileWriteTeste.java
@@ -0,0 +1,17 @@
+package filewirteteste;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriteTeste {
+    public static void main(String[] args) {
+        File file = new File("file.txt");
+        try(FileWriter fw = new FileWriter(file)) {
+            fw.write("Eu serei um programador e irei morar na alemanha");
+            fw.flush();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
